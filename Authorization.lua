d7f13fd5c067ffd68e320625ea7e8233e423da3d
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-02-08 02:31:32 +0800

    Create Authorization.lua

---
+++ Authorization.lua
@@ -0,0 +1,19 @@
+local authorizedUsers = {
+    "jerfie22",
+    "Dangblakkaye",
+    "ImGrindingDontWakeMe"
+}
+
+local player = game.Players.LocalPlayer
+local isAuthorized = false
+
+for _, username in ipairs(authorizedUsers) do
+    if player.Name == username then
+        isAuthorized = true
+        break
+    end
+end
+
+if not isAuthorized then
+    player:Kick("Buy: https://discord.com/invite/AK5mmqDVWe")
+end
