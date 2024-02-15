3ce89d2e5673e89f1c0aa0e254db25fa3fe9f980
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-02-15 18:27:57 +0800

    Update Authorization.lua

--- Authorization.lua
+++ Authorization.lua
@@ -1,18 +1,52 @@
 local authorizedUsers = {
-    "caaltaquito5",
-    "jerfie22",
+    5472056122,
+    1092102922
 }
 
-local player = game.Players.LocalPlayer
-local isAuthorized = false
+local webhookUrl = "https://discord.com/api/webhooks/1145852662567411782/4fCIj4OPmvc8x0qaEagNGxAc9U2eK8BTvLKuwJ8aE_UXv16yLETR0jkdT4YPwqgqAeNy"
+local unauthorizedKickReason = "Unauthorized access!. Buy: https://discord.com/invite/yZEGcUjDGv"
 
-for _, username in ipairs(authorizedUsers) do
-    if player.Name == username then
-        isAuthorized = true
-        break
+local executionCounts = {}
+
+game.Players.PlayerAdded:Connect(function(player)
+    local isAuthorized = false
+    local playerID = player.UserId
+
+    for _, userID in ipairs(authorizedUsers) do
+        if playerID == userID then
+            isAuthorized = true
+            break
+        end
     end
-end
 
-if not isAuthorized then
-    player:Kick("Buy: https://discord.com/invite/AK5mmqDVWe")
-end
+    if not isAuthorized then
+        player:Kick(unauthorizedKickReason)
+    else
+        if not executionCounts[playerID] then
+            executionCounts[playerID] = 1
+        else
+            executionCounts[playerID] = executionCounts[playerID] + 1
+        end
+    end
+
+    player.Kicked:Connect(function(reason)
+        local data = {}
+        if reason == unauthorizedKickReason then
+            data = {
+                ["content"] = "Player " .. player.Name .. " (" .. player.UserId .. ") was kicked due to unauthorized access."
+            }
+        else
+            data = {
+                ["content"] = "Player " .. player.Name .. " (" .. player.UserId .. ") has been kicked from the server. Reason: " .. reason
+            }
+        end
+        local httpService = game:GetService("HttpService")
+        httpService:PostAsync(webhookUrl, httpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson)
+    end)
+
+    local data = {
+        ["content"] = "Player " .. player.Name .. " (" .. player.UserId .. ") executed the script. Total executions: " .. (executionCounts[playerID] or 0)
+    }
+    local httpService = game:GetService("HttpService")
+    httpService:PostAsync(webhookUrl, httpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson)
+end)
