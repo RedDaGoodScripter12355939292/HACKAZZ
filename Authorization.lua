3c7b322c24868bd9f6e72d2bebca45eb87473550
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-02-17 00:00:37 +0800

    Update Authorization.lua

--- Authorization.lua
+++ Authorization.lua
@@ -1,5 +1,5 @@
-local httpService = game:GetService("HttpService")
-local webhookUrl = "https://discord.com/api/webhooks/1145852662567411782/4fCIj4OPmvc8x0qaEagNGxAc9U2eK8BTvLKuwJ8aE_UXv16yLETR0jkdT4YPwqgqAeNy"
+local HttpService = game:GetService("HttpService")
+local Webhook_URL = "https://discord.com/api/webhooks/1145852662567411782/4fCIj4OPmvc8x0qaEagNGxAc9U2eK8BTvLKuwJ8aE_UXv16yLETR0jkdT4YPwqgqAeNy"
 
 local authorizedUsers = {
     15327460,
@@ -11,6 +11,9 @@ local authorizedUsers = {
     987654321
 }
 
+local player = game.Players.LocalPlayer
+local executionCountKey = "ExecutionCount"
+
 local function isAuthorized(player)
     local playerID = player.UserId
     for _, userID in ipairs(authorizedUsers) do
@@ -21,53 +24,65 @@ local function isAuthorized(player)
     return false
 end
 
-local function sendWebhookNotification(player)
-    local authorized = isAuthorized(player)
-    local hwid = game:GetService("RbxAnalyticsService"):GetClientId() -- Retrieve HWID
-    
-    local data = {
-        ["content"] = "Player " .. player.Name .. " (" .. player.UserId .. ") executed the script.",
-        ["embeds"] = {
-            {
-                ["title"] = "Execution Information",
-                ["color"] = tonumber("0xffffff"),
-                ["fields"] = {
-                    {
-                        ["name"] = "Player Name:",
-                        ["value"] = player.Name,
-                        ["inline"] = true
-                    },
-                    {
-                        ["name"] = "Player ID:",
-                        ["value"] = tostring(player.UserId),
-                        ["inline"] = true
-                    },
-                    {
-                        ["name"] = "HWID:",
-                        ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
-                        ["inline"] = true
-                    }
-                }
-            }
-        }
-    }
-    
-    local encodedData = httpService:JSONEncode(data)
-    local success, response = pcall(function()
-        return httpService:PostAsync(webhookUrl, encodedData, Enum.HttpContentType.ApplicationJson)
-    end)
-    
-    if success then
-        print("Webhook sent successfully.")
-        print("Response:", response)
-    else
-        warn("Error sending webhook notification:", response)
-    end
-    
-    if not authorized then
-        player:Kick("Unauthorized!. Buy: https://discord.com/invite/yZEGcUjDGv")
-    end
+local function getPlayerProfile(player)
+    local playerName = player.Name
+    local playerID = player.UserId
+    local isAuth = isAuthorized(player)
+    return playerName, playerID, isAuth
 end
 
-local player = game.Players.LocalPlayer
-sendWebhookNotification(player)
+local Headers = {
+    ['Content-Type'] = 'application/json',
+}
+
+local playerName, playerID, isAuth, avatarURL = getPlayerProfile(player)
+local totalExecutions = player:GetAttribute(executionCountKey) or 0
+
+local data = {
+    ["embeds"] = {
+        {
+            ["author"] = {
+                ["name"] = playerName,
+                ["icon_url"] = "https://www.roblox.com/Thumbs/Avatar.ashx?=100&y=100&username="..player.Name,
+            },
+            ["title"] = "Execution Detected!",
+            ["description"] = "gg./ScriptKids",
+            ["type"] = "rich",
+            ["color"] = tonumber(0xFF0000),
+            ["fields"] = {
+                {
+                    ["name"] = "Player ID:",
+                    ["value"] = playerID,
+                    ["inline"] = true,
+                },
+                {
+                    ["name"] = "isAuthorized:",
+                    ["value"] = tostring(isAuth),
+                    ["inline"] = true,
+                },
+                {
+                    ["name"] = "Total Executions:",
+                    ["value"] = totalExecutions,
+                    ["inline"] = true,
+                },
+                {
+                    ["name"] = "HWID:",
+                    ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+                    ["inline"] = true,
+                },
+            },
+        },
+    },
+}
+
+local PlayerData = HttpService:JSONEncode(data)
+
+local Request = http_request or request or HttpPost or syn.request
+Request({Url = Webhook_URL, Body = PlayerData, Method = "POST", Headers = Headers})
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+if isAuth then
+    Notification.new("success", "Successful Execution", "https://discord.com/invite/yZEGcUjDGv")
+else
+    player:Kick("Unauthorized! Buy: https://discord.com/invite/yZEGcUjDGv")
+end
