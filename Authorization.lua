5353eee724d955bf6568e5c1259c5f1ffa3c70ca
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-03-26 18:40:19 +0800

    Update authorization script

--- Authorization.lua
+++ Authorization.lua
@@ -18,7 +18,6 @@ local Webhook_URL = "https://discord.com/api/webhooks/1145852662567411782/4fCIj4
 
 local authorizedUsers = {
     5469778938,
-    3937090405,
     5376607871,
     1860037433,
     5706994650,
