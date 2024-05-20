e1cbf1132ba31c6f4cf527012eee6d1fac16537a
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-05-21 01:39:10 +0800

    Update authorization script

--- Authorization.lua
+++ Authorization.lua
@@ -17,6 +17,7 @@ local HttpService = game:GetService("HttpService")
 local Webhook_URL = "https://discord.com/api/webhooks/1223593819547766814/ssVKh18Wpm1E_Xy1jbNxO_SCqNq4Yu4Xqi9WDk6odGtySEhrWiOnEGDTYRwr9IiEqTzq"
 
 local authorizedUsers = {
+    5390642682,
     5476298709,
     5395817623,
     060911123
