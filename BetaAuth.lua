acedca8b947b8afa5da277e88e1baa4808aff421
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-08-23 18:19:30 +0800

    Update BetaAuth.lua

--- BetaAuth.lua
+++ BetaAuth.lua
@@ -17,8 +17,7 @@ local HttpService = game:GetService("HttpService")
 local Webhook_URL = "https://discord.com/api/webhooks/1223593819547766814/ssVKh18Wpm1E_Xy1jbNxO_SCqNq4Yu4Xqi9WDk6odGtySEhrWiOnEGDTYRwr9IiEqTzq"
 
 local authorizedUsers = {
-    7154133006,
-    7867444,
+    3713791320,
     060911123
 }
 
