b4f543d54e43ffe73097e210403052f01462953f
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-08-24 14:22:11 +0800

    Update BetaAuth.lua

--- BetaAuth.lua
+++ BetaAuth.lua
@@ -109,5 +109,5 @@ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 if isAuth then
     Notification.new("success", "Successful Execution", "https://discord.com/invite/yZEGcUjDGv")
 else
-    player:Kick("Script is only for beta testers!")
+    player:Kick("§§§ERROR!!§§ §§§§CLASSIFIED META ONLY§§§§")
 end
