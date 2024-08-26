1854e6493554fb65886c556d97c6f3a9c3bc125c
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-08-26 22:13:22 +0800

    Update BetaAuth.lua

--- BetaAuth.lua
+++ BetaAuth.lua
@@ -19,7 +19,7 @@ local Webhook_URL = "https://discord.com/api/webhooks/1223593819547766814/ssVKh1
 local authorizedUsers = {
     3713791320,
     1937193879,
-    060911123
+    41167709
 }
 
 local player = game.Players.LocalPlayer
