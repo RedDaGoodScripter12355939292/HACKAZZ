e2f4820253c7e216ad52436bfe4a19d345abbb4e
AaronScripts <118042993+RedDaGoodScripter12355939292@users.noreply.github.com>
2024-11-18 21:49:04 +0800

    Update Trial.lua

--- Trial.lua
+++ Trial.lua
@@ -1,8 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RedDaGoodScripter12355939292/HACKAZZ/main/Authorization.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RedDaGoodScripter12355939292/HACKAZZ/main/BetaAuth.lua"))()
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 
 local success, result = pcall(function()
-    Notification.new("success", "Successful Execution", "Lola_ruu-g5ai")
+    Notification.new("success", "Successful Execution", "Lola_ruu-g5ai CP")
 
     -- GLOBAL SETTINGS
     getgenv().Settings = {
